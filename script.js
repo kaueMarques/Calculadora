fa2bc8c5b2f74f3b973281ba550b73d6be1d0fc4
EDnotSheeran <juninho.tab3@gmail.com>
2020-03-19 17:13:43 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const visor = document.querySelector('#visor')
 const botoes = document.querySelector('#botoes')
+const calculadora = document.querySelector('#calculadora')
 
-const btn_value = ['ac','+/-','%','&divide;','7','8','9','&times;','4','5','6','&minus;','1','2','3','&plus;','0',',','=']
+const btn_value = ['ac','+/-','%','÷','7','8','9','×','4','5','6','−','1','2','3','+','0',',','=']
 
 btn_value.map(value=>{
     const btn = document.createElement('button')
@@ -12,8 +13,41 @@ btn_value.map(value=>{
         btn.classList = btn.classList + '-largo'
     if(value=='ac'|value=='+/-'|value=='%')
         btn.classList = btn.classList + '-cinza'
-    if(value=='&divide;'|value=='&times;'|value=='&minus;'|value=='&plus;'|value=='=')
+    if(value=='÷'|value=='×'|value=='−'|value=='+'|value=='=')
         btn.classList = btn.classList + '-laranja'
+    
+    if(value == '=')
+        btn.type = 'submit'
+    else if(value == 'ac')
+        btn.addEventListener('click', btn=>{
+            btn.preventDefault()
+            visor.innerHTML = ''
+        })
+    else
+        btn.addEventListener('click', btn=>{
+            btn.preventDefault()
+            visor.innerHTML = visor.innerHTML + value
+        })
         
     botoes.appendChild(btn)
 })
+
+calculadora.addEventListener('submit',form=>{
+    form.preventDefault()
+    
+    visor.innerHTML = calcular(visor.value)
+})
+
+function calcular(query){
+    query = query.replace('÷','/')
+    query = query.replace('×','*')
+    query = query.replace('−','-')
+    query = query.replace('+','+')
+
+    try {
+        var resposta = eval(query)
+    } catch (error) {
+        var resposta = 'Error'
+    }
+    return resposta
+}
